571925c3ef2008a119e3dc48877e734d26720eb3
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2026-05-01 12:15:32 +0530

    Add Practical 7: Web Service Client

---
+++ Practical_7_WebService/HelloWorldClient.java
@@ -0,0 +1,15 @@
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+public class HelloWorldClient {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://localhost:7779/ws/hello?wsdl");
+        QName qname = new QName("http://DefaultNamespace/", "HelloWorldImplService");
+
+        Service service = Service.create(url, qname);
+        HelloWorld hello = service.getPort(HelloWorld.class);
+
+        System.out.println(hello.getHelloWorldAsString("SPPU Student"));
+    }
+}

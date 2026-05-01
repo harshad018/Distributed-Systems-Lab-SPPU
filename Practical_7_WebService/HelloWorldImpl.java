00187da25c29048ac6dbd3844ac21ff4db7f67ed
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2026-05-01 12:15:22 +0530

    Add Practical 7: Web Service Implementation

---
+++ Practical_7_WebService/HelloWorldImpl.java
@@ -0,0 +1,9 @@
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "HelloWorld")
+public class HelloWorldImpl implements HelloWorld {
+    @Override
+    public String getHelloWorldAsString(String name) {
+        return "Hello World JAX-WS " + name;
+    }
+}
